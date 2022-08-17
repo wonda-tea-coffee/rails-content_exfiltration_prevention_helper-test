60dba7b85b78c0b493e531e523fea756230cda98
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2022-08-17 19:40:36 +0900

    toggle config.action_view.prepend_content_exfiltration_prevention

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_view.prepend_content_exfiltration_prevention = true
   end
 end
